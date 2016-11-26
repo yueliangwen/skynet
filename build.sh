636af24a454cdd1760c2adc22352eaa8cc91d552
ylw <yueliangwen@vip.163.com>
2016-11-26 17:32:20 +0800

    1、给skynet增加CMake编译支持，但是不支持编译jemalloc。 2、通过支持CMake构建，能使用Clion等IDE(visualstudio 2017也增加了对CMake的支持)进行调试开发。 3、发布release时为了集成jemalloc，可以沿用make构建。 4、TODO：通过build.sh增加对jemalloc编译支持。

---
+++ build.sh
@@ -0,0 +1,118 @@
+#!/bin/sh
+
+#####################################################################################
+#  @Filename: build.sh
+#  @Brief   : 
+#  @Auth    : chenx
+#  @Date    : 2013-12-19
+#  @Version : ver 1.0
+#  @Function: 
+#  @Inparam : NA
+#  @Outparam: NA
+#  @Remark  : NA
+#####################################################################################
+
+BUILD_PATH="./build"
+
+#####################################################################################
+#  @Brief   : 简单日志
+#  @Inparam : NA
+#  @Outparam: NA
+#  @Remark  : NA
+#####################################################################################
+function Log
+{
+    echo "[BUILD] " `date +"%Y-%m-%d %H:%M:%S"` "    " $1;
+}
+
+#####################################################################################
+#  @Brief   : 检查是否存在外部编译目录不存在则创建
+#  @Inparam : NA
+#  @Outparam: NA
+#  @Remark  : NA
+#####################################################################################
+function CreateBuildDirectory
+{
+    Log "checking build path..."
+    if [ ! -x "${BUILD_PATH}" ]; then
+        Log "creating build path..."
+        mkdir "${BUILD_PATH}"
+        ##由于权限可能导致创建失败，将在使用该目录时候检查
+    fi
+}
+
+#####################################################################################
+#  @Brief   : 打印帮助信息
+#  @Inparam : NA
+#  @Outparam: NA
+#  @Remark  : NA
+#####################################################################################
+function PrintUsage
+{
+    echo "Usage:"
+    echo "  $0 {release|debug|all|clean}"
+    echo "  - realse     cmake .."
+    echo "  - debug      cmake -DCMAKE_BUILD_TYPE=Debug -DUSER_DEBUG=1 .."
+    echo "  - all        cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_GCOV=1 -DUSER_DEBUG=1 .."
+    echo "  - clean      clean .."
+}
+
+#####################################################################################
+#  @Brief   : 编译目标文件
+#  @Inparam : cmake 参数
+#  @Outparam: NA
+#  @Remark  : NA
+#####################################################################################
+function BuildTarget
+{
+    Log "access ${BUILD_PATH}"
+    cd ${BUILD_PATH}
+    if [ $? -eq 0 ]; then
+        Log "execute cmake $1"
+        cmake $1
+        if [ $? -eq 0 ]; then
+            make
+        else
+            Log "execute cmake $1 failed!"
+        fi
+    else
+        Log "access ${BUILD_PATH} failed!"
+    fi
+}
+
+#####################################################################################
+#  @Brief   : 主函数
+#  @Inparam : NA
+#  @Outparam: NA
+#  @Remark  : NA
+#####################################################################################
+function Main
+{
+    case "x$1" in
+        xrelease)
+            CreateBuildDirectory
+            BuildTarget ".."
+            ;;
+        xdebug)
+            CreateBuildDirectory
+            BuildTarget "-DCMAKE_BUILD_TYPE=Debug -DUSER_DEBUG=1 .."
+            ;;
+        xall)
+            CreateBuildDirectory;
+            BuildTarget "-DCMAKE_BUILD_TYPE=Debug -DENABLE_GCOV=1 -DUSER_DEBUG=1 .."
+            ;;
+        xclean)
+            Log "cleaning..."
+
+            if [ -x "${BUILD_PATH}" ]; then
+                rm -rf "${BUILD_PATH}";
+            fi
+            ;;
+        *)
+            PrintUsage;
+            ;;
+    esac
+}
+
+## Entrance...
+Main $1
